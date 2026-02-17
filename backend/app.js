58df7b7dc2a29dd015c958068b1769ae7cc0c525
tarinievinka <arumathanthirige14@gmail.com>
2026-02-17 19:49:23 +0530

    mongoDB configuration and install dev dependency

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,20 @@
 require('dotenv').config(); // Load variables from .env
 const express = require('express');
 const app = express();
+const connectDB = require('./src/config/db');
 
-// Use the port from .env, or fallback to 3000 if not found
-const port = process.env.PORT || 5000; 
+// Use the port from .env, or fallback to 5000 if not found
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Server running with .env port!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+const start = async () => {
+  await connectDB();
+  app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+  });
+};
+
+start();
